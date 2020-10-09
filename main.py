39e15eec47f9071211c4b3cbd6482843b1ab885e
TNtube <stanis.kapus@gmail.com>
2020-10-08 22:48:44 -0300

    Adding config help system. Add also the possibility to change the name and the owner of the voice channel.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 import os
 import json
 
@@ -23,6 +24,12 @@ bdd = {}
 async def on_ready():
     update_bdd(bdd)
     print(f"Bot logged as {bot.user.name}")
+    for guild in bot.guilds:
+        if str(guild.id) not in bdd.keys():
+            bdd[str(guild.id)] = {"main": None, "channels": {}}
+    game = discord.Game(f"Manage {len(bot.guilds)} servers")
+    await bot.change_presence(activity=game)
+    update_json(bdd)
 
 
 @bot.event
@@ -41,13 +48,82 @@ async def setchannel(ctx):
                 chan = bot.get_channel(int(content[1]))
                 if chan:
                     bdd[str(ctx.guild.id)]['main'] = chan.id
-                    await ctx.send("Channel principal configuré")
+                    await ctx.send("Channel principal configuré.")
                 else:
-                    await ctx.send("Veuillez envoyer une id valide")
+                    await ctx.send("Veuillez envoyer une id valide.")
             else:
-                await ctx.send("Veuillez envoyer une id valide")
+                await ctx.send("Veuillez envoyer une id valide.")
         else:
-            await ctx.send("Veuillez spécifier un channel id")
+            await ctx.send("Veuillez spécifier un channel id.")
+    update_json(bdd)
+    update_bdd(bdd)
+
+
+def help_embed(member):
+    embed = discord.Embed(
+        title="Config help",
+        color=discord.Color(5027441)
+        # f"https://cdn.discordapp.com/avatars/{member.id}/{member.avatar}.png?size=1024"
+    )
+    embed.set_thumbnail(url=f"https://cdn.discordapp.com/avatars/{bot.user.id}/{bot.user.avatar}.png?size=1024")
+    embed.add_field(
+        name="__owner__",
+        value="**Description:** allows you to change the owner\n**Usage:**: `config owner [mention]`",
+        inline=False
+    )
+    embed.add_field(
+        name="__name__",
+        value="**Description:** allows you to change the channel's name\n**Usage:**: `config name [name]`",
+        inline=False
+    )
+    embed.add_field(
+        name="__places__",
+        value="**Description:** allows you to change the number of places in the channel. "
+              "Take a number between 0 to 99, 0 mean no limit.\n**Usage:**: `config places [int]`",
+        inline=False
+    )
+    embed.add_field(
+        name="__public/private__",
+        value="**Description:** allows you to define the channel as public or private\n"
+              "**Usage:**: `config private` or `config public`",
+        inline=False
+    )
+    embed.add_field(
+        name="__help__",
+        value="**Description:** Show you this embed\n**Usage:**: `config help`",
+        inline=False
+    )
+    return embed
+
+
+@bot.command()
+async def config(ctx):
+    content = ctx.message.content.split()
+    voice = ctx.author.voice
+    member_id = ctx.author.id
+    guild_id = str(ctx.guild.id)
+    keywords = ["private", "public", "owner", "places", "name", "help"]
+    if voice:
+        chan = voice.channel
+        voice = str(voice.channel.id)
+        if voice in bdd[guild_id]['channels'].keys():
+            if member_id == bdd[guild_id]['channels'][voice]["owner"]:
+                if len(content) > 1 and content[1] in keywords:
+                    if content[1] == 'name' and len(content) > 2:
+                        await chan.edit(name=" ".join(content[2:]))
+                        await ctx.message.channel.send("Nom changé avec succés")
+                        bdd[guild_id]['channels'][voice]["name"] = " ".join(content[2:])
+                    if content[1] == "owner" and len(ctx.message.mentions):
+                        bdd[guild_id]['channels'][voice]["owner"] = ctx.message.mentions[0].id
+                else:
+                    await ctx.message.channel.send(embed=help_embed(ctx.author))
+
+            else:
+                await ctx.message.channel.send("Vous n'avez pas la permission !")
+        else:
+            await ctx.message.channel.send('Vous devez être dans votre channel vocal.')
+    else:
+        await ctx.message.channel.send('Vous devez être dans votre channel vocal.')
     update_json(bdd)
     update_bdd(bdd)
 
