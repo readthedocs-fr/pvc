b1e5328c313002487bf4f58748f68fec38cd77ab
TNtube <stanis.kapus@gmail.com>
2020-10-09 23:44:49 -0300

    Adding README.md. Finish lasts essential commands to edit channels.

---
+++ README.md
@@ -0,0 +1,29 @@
+[en]\
+\
+Here is the source code of the `Private VoIP Channels#7454` Bot. Thanks to this bot, 
+you will be able to set up a channel to create private voice channels.
+Indeed, with the `$setchannel [channel_id]` command, you can configure this channel
+so that the next people connecting to it can have their own voice channel.
+
+With the possibility to change the name of the channel, the number of places, to put it in private or in public,
+you will be able to change your own channel without danger for the server.
+
+To be able to invite the bot on your server, you can pass 
+by this [link](https://discord.com/oauth2/authorize?client_id=764311685355274251&scope=bot&permissions=16777232)
+
+
+[fr]\
+\
+Voici le code source du Bot `Private VoIP Channels#7454`. Grâce à ce bot, 
+vous pourrez mettre en place un channel permettant de créer des channels vocaux privés.
+En effet, avec la commande `$setchannel [channel_id]`, vous pourrez configurer ce dit channel
+afin que les prochains se connectant dessus puissent avoir leur propre channel vocal.
+
+Avec la possibilité de changer le nom du channel, le nombre de place, de le mettre en privé ou en public,
+vous pourrez modifier votre propre channel sans danger pour le serveur.
+
+Pour pouvoir inviter le bot sur votre serveur, vous pouvez passer 
+par ce [lien](https://discord.com/oauth2/authorize?client_id=764311685355274251&scope=bot&permissions=16777232)
+
+<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from 
+<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
 import os
 import json
 
-bot = commands.Bot(command_prefix="%")
+bot = commands.Bot(command_prefix="$")
+bot.remove_command('help')
 
 
 def update_json(db: dict):
@@ -39,70 +40,106 @@ async def on_guild_join(guild):
     update_json(bdd)
 
 
+@bot.command(name="help")
+async def _help(ctx):
+    embed = discord.Embed(
+        title="Help",
+        color=discord.Color(5027441)
+    )
+    embed.set_thumbnail(url=f"https://cdn.discordapp.com/avatars/{bot.user.id}/{bot.user.avatar}.png?size=1024")
+    embed.add_field(
+        name="__help__",
+        value="**Description:** Show you this message.\n**Usage:**: `help`",
+        inline=False
+    )
+    embed.add_field(
+        name="__setchannel__",
+        value="**Description:** allows you to assign a main channel.\n**Usage:**: `setchannel [channel_id]`",
+        inline=False
+    )
+    embed.add_field(
+        name="__unsetchannel__",
+        value="**Description:** reset the main channel.\n**Usage:**: `unsetchannel`",
+        inline=False
+    )
+    await ctx.send(embed=embed)
+
+
 @bot.command()
+@commands.has_permissions(administrator=True)
 async def setchannel(ctx):
     content = ctx.message.content.split()
-    if not bdd[str(ctx.guild.id)]['main']:
-        if len(content) > 1:
-            if content[1].isdigit():
-                chan = bot.get_channel(int(content[1]))
-                if chan:
+    if len(content) > 1:
+        if content[1].isdigit():
+            chan = bot.get_channel(int(content[1]))
+            if chan:
+                if isinstance(chan, discord.VoiceChannel):
                     bdd[str(ctx.guild.id)]['main'] = chan.id
-                    await ctx.send("Channel principal configuré.")
+                    await ctx.send("Main channel set.")
                 else:
-                    await ctx.send("Veuillez envoyer une id valide.")
+                    await ctx.send("Please use a Vocal channel id.")
             else:
-                await ctx.send("Veuillez envoyer une id valide.")
+                await ctx.send("Please use a valid id.")
         else:
-            await ctx.send("Veuillez spécifier un channel id.")
+            await ctx.send("Please use a valid id.")
+    else:
+        await ctx.send("Please specify a channel id.")
+    update_json(bdd)
+    update_bdd(bdd)
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def unsetchannel(ctx):
+    bdd[str(ctx.guild.id)]['main'] = None
+    await ctx.send("Main channel reset.")
     update_json(bdd)
     update_bdd(bdd)
 
 
 def help_embed(member):
     embed = discord.Embed(
-        title="Config help",
+        title="Setings help",
         color=discord.Color(5027441)
-        # f"https://cdn.discordapp.com/avatars/{member.id}/{member.avatar}.png?size=1024"
     )
-    embed.set_thumbnail(url=f"https://cdn.discordapp.com/avatars/{bot.user.id}/{bot.user.avatar}.png?size=1024")
+    embed.set_thumbnail(url=f"https://cdn.discordapp.com/avatars/{member.id}/{member.avatar}.png?size=1024")
     embed.add_field(
         name="__owner__",
-        value="**Description:** allows you to change the owner\n**Usage:**: `config owner [mention]`",
+        value="**Description:** allows you to change the owner\n**Usage:**: `set owner [mention]`",
         inline=False
     )
     embed.add_field(
         name="__name__",
-        value="**Description:** allows you to change the channel's name\n**Usage:**: `config name [name]`",
+        value="**Description:** allows you to change the channel's name\n**Usage:**: `set name [name]`",
         inline=False
     )
     embed.add_field(
         name="__places__",
-        value="**Description:** allows you to change the number of places in the channel. "
-              "Take a number between 0 to 99, 0 mean no limit.\n**Usage:**: `config places [int]`",
+        value="**Description:** allows you to change the number of places in the channel.\n"
+              "Take a number between 0 to 99, 0 mean no limit.\n**Usage:**: `set places [int]`",
         inline=False
     )
     embed.add_field(
         name="__public/private__",
         value="**Description:** allows you to define the channel as public or private\n"
-              "**Usage:**: `config private` or `config public`",
+              "**Usage:**: `config private` or `set public`",
         inline=False
     )
     embed.add_field(
         name="__help__",
-        value="**Description:** Show you this embed\n**Usage:**: `config help`",
+        value="**Description:** Show you this embed\n**Usage:**: `set help`",
         inline=False
     )
     return embed
 
 
 @bot.command()
-async def config(ctx):
+async def set(ctx):
     content = ctx.message.content.split()
     voice = ctx.author.voice
     member_id = ctx.author.id
     guild_id = str(ctx.guild.id)
-    keywords = ["private", "public", "owner", "places", "name", "help"]
+    keywords = ["private", "public", "owner", "places", "name"]
     if voice:
         chan = voice.channel
         voice = str(voice.channel.id)
@@ -111,19 +148,37 @@ async def config(ctx):
                 if len(content) > 1 and content[1] in keywords:
                     if content[1] == 'name' and len(content) > 2:
                         await chan.edit(name=" ".join(content[2:]))
-                        await ctx.message.channel.send("Nom changé avec succés")
+                        await ctx.message.channel.send("Name successfully changed.")
                         bdd[guild_id]['channels'][voice]["name"] = " ".join(content[2:])
-                    if content[1] == "owner" and len(ctx.message.mentions):
+                    elif content[1] == "owner" and len(ctx.message.mentions):
                         bdd[guild_id]['channels'][voice]["owner"] = ctx.message.mentions[0].id
+                        await ctx.message.channel.send(f"Owner successfully changed to {ctx.message.mentions[0].name}.")
+                    elif content[1] == "places":
+                        if content[2].isdigit():
+                            if 0 <= int(content[2]) < 100:
+                                await chan.edit(user_limit=int(content[2]))
+                                await ctx.send("Number of places successfully changed.")
+                                bdd[guild_id]['channels'][voice]["places"] = int(content[2])
+                            else:
+                                await ctx.send("Please use a number between 0 and 99.")
+                        else:
+                            await ctx.send("Please use a number between 0 and 99.")
+                    elif content[1] == "public":
+                        await chan.set_permissions(ctx.guild.default_role, view_channel=True)
+                        await ctx.send("Channel defined as public.")
+                        bdd[guild_id]['channels'][voice]["public"] = True
+                    else:
+                        await chan.set_permissions(ctx.guild.default_role, view_channel=False)
+                        await ctx.send("Channel defined as private.")
+                        bdd[guild_id]['channels'][voice]["public"] = False
                 else:
-                    await ctx.message.channel.send(embed=help_embed(ctx.author))
-
+                    await ctx.send(embed=help_embed(ctx.author))
             else:
-                await ctx.message.channel.send("Vous n'avez pas la permission !")
+                await ctx.send("You don't have permission to do this !")
         else:
-            await ctx.message.channel.send('Vous devez être dans votre channel vocal.')
+            await ctx.send('You must be in your voice channel.')
     else:
-        await ctx.message.channel.send('Vous devez être dans votre channel vocal.')
+        await ctx.send('You must be in your voice channel.')
     update_json(bdd)
     update_bdd(bdd)
 
@@ -139,7 +194,7 @@ async def on_voice_state_update(member, before, after):
                 "owner": member.id,
                 "name": change.name,
                 "public": True,
-                "places": 2
+                "places": 0
             }
     if before.channel and str(before.channel.id) in bdd[str(member.guild.id)]["channels"] \
             and not len(before.channel.members):
